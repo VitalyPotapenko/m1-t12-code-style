5c1808e94596d92e25d23a070c9dd0e2594cb6ae
grand1964 <121289986+grand1964@users.noreply.github.com>
2023-01-26 11:08:43 +0300

    Update DepositCalculator.java

--- src/DepositCalculator.java
+++ src/DepositCalculator.java
@@ -3,15 +3,17 @@ import java.util.Scanner;
 public class DepositCalculator {
     double calculateComplexPercent(double amount, double yearRate, int depositPeriodInYears) {
         double percentageReturn = amount * Math.pow((1 + yearRate / 12), 12 * depositPeriodInYears);
+        //depositPeriodInYears - удачное имя
 
-        return roundValueToCertainNumberOfDigits(percentageReturn, 2);
+        return roundValueToCertainNumberOfDigits(percentageReturn, 2); //пустая строка перед return вроде не предполагается
     }
 
     double calculateSimplePercent(double amount, double yearRate, int depositPeriodInYears) {
         double pay = amount + amount * yearRate * depositPeriodInYears;
-        return roundValueToCertainNumberOfDigits(pay, 2);
+        return roundValueToCertainNumberOfDigits(pay, 2); //может, лучше использовать именованную константу?
     }
 
+    //надо бы в названии как-то отметить, что округляются цифры именно после запятой; но я и сам не придумал, как бы это покороче назвать
     double roundValueToCertainNumberOfDigits(double value, int places) {
         double scale = Math.pow(10, places);
 
@@ -20,7 +22,7 @@ public class DepositCalculator {
 
     void calculateDepositSum() {
         int period;
-        int action;
+        int action; //не совсем удачно: это - правило расчета процентов, а не действие
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите сумму вклада в рублях:");
@@ -29,9 +31,9 @@ public class DepositCalculator {
         period = scanner.nextInt();
         System.out.println("Выберите тип вклада, 1 - вклад с обычным процентом, 2 - вклад с капитализацией:");
         action = scanner.nextInt();
-        double depositIncome = 0;
+        double depositIncome = 0; //тоже не очень удачно: в этой переменной не входящая, а итоговая сумма
         if (action == 1) {
-            depositIncome = calculateSimplePercent(amount, 0.06, period);
+            depositIncome = calculateSimplePercent(amount, 0.06, period); //процентную ставку тоже лучше задать константой
         } else if (action == 2) {
             depositIncome = calculateComplexPercent(amount, 0.06, period);
         }
